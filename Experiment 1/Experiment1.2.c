ef2fa7f2f1fc9e6742167d2c34ebb92eb2943e8f
Ayush-dev2 <ayunegi651@gmail.com>
2025-08-31 11:17:08 +0530

    Add files via upload

---
+++ Experiment 1/Experiment1.2.c
@@ -0,0 +1,12 @@
+//2./* Write a C Program to print the address in multiple lines (new line)*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Special wing\n");
+    printf("House no 11\n");
+    printf("Special wing\n");
+    printf("State - Utttrakhand\n");
+    printf("ZIP Code - 248007\n");
+    return 0;
+}
